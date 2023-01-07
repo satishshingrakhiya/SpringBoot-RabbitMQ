f790998029a2b818ab25c8e294218ce1dd1f7ec2
Satish Shingrakhiya <satish.shingrakhiy@gmail.com>
2023-01-07 19:52:42 +0530

    Create Producer

---
+++ Producer/src/test/java/com/stasih/Producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stasih.Producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
